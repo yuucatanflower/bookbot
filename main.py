b536a7baa90eab2dd92cdf4ab7aa22b6c7920998
yuucatanflower <maksym_zaremskyy@yahoo.com>
2025-01-25 23:19:15 +0100

    final fix

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def book():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
     return file_contents
 
 def count_words(book):
     l = len(book.split())
-    print(f"The book contains {l} words.")
+    print(f"{l} words found in the document")
 
 def sort_on(dict):
     return dict["count"]
@@ -33,14 +32,22 @@ def count_characters(book):
     char_list.sort(reverse=True, key=sort_on)
 
     for c in char_list:
-        if c["char"].isalpha():
-            print(f"The '{c["char"]}' character was found {c["count"]} times")
+        if c['char'].isalpha():
+            print(f"The '{c['char']}' character was found {c['count']} times")
 
     return char_list
 
 
-book()
+# print(book())
 
-count_words(book())
+# count_words(book())
 
-count_characters(book())
+def main():
+    print("--- Begin report of books/frankenstein.txt ---")
+    text = book()  # store the book contents once
+    count_words(text)
+    print()  # blank line
+    count_characters(text)  # this should print all characters including 'e'
+    print("--- End report ---")
+
+main()  # only call this once

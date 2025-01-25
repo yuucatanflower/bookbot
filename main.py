742a9f200adc6f98260bcc1f65cbeb6a9765c263
yuucatanflower <maksym_zaremskyy@yahoo.com>
2025-01-25 22:23:05 +0100

    word counter

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
+def book():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    return file_contents
 
-main()
+def count_words(book):
+    l = len(book)
+    print(f"The book contains {l} words.")
+
+book()
+
+count_words(book().split())

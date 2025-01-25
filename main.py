de9a5d589c6ae1df97c304ed70d6558f1c7b820f
yuucatanflower <maksym_zaremskyy@yahoo.com>
2025-01-25 22:35:29 +0100

    character counter

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ def book():
     return file_contents
 
 def count_words(book):
-    l = len(book)
+    l = len(book.split())
     print(f"The book contains {l} words.")
+
+def count_characters(book):
+    lowercase = book.lower()
+    char_dic = {
+
+    }
+
+    for word in lowercase:
+        for char in word:
+            if char not in char_dic:
+                char_dic[char]=1
+            else: char_dic[char]+=1
+    return char_dic
+
 
 book()
 
-count_words(book().split())
+count_words(book())
+
+print(count_characters(book()))
